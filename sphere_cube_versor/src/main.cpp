c04d5f831630398b7715ddf5d748507e09e618c8
sosterwalder <sven.osterwalder@gmail.com>
2013-12-22 22:52:15 +0100

    Implementing of main project goal
    * Implemented main goal of project

--- sphere_cube_versor/src/main.cpp
+++ sphere_cube_versor/src/main.cpp
@@ -9,6 +9,8 @@
 using namespace vsr;
 using namespace std;
 
+#pragma GCC diagnostic ignored "-Wswitch"
+
 static long timeGetTime( void )
 {
     struct timeval now; //, res;
@@ -18,29 +20,52 @@ static long timeGetTime( void )
     return (long)((now.tv_sec*1000) + (now.tv_usec/1000));
 }
 
-/*template< class B >
-auto nrot( const B& b) -> decltype( B() + 1 ) {         
-    VT c = sqrt(-(b.wt()));
-    VT sc = -sin(c);
-
-    if (c != 0) {
-        sc /= c;
-    }
-   
-    return (b*sc) + cos(c);
-} */
-
 struct MyApp : App {    
+    static const int DIM = 4;
 
     Pnt mouse;
     Lin ray;
     long to;
     float fTime;
-
+    NCube<DIM> cube;
+    vector<Vec> vecCube;
+    Biv bivRotation;
+    bool bDoAnimate;
+    bool bDrawSpheresAsWireframe;
+    bool bDrawCubeAsLines;
+    enum CubeRenderModes { NORMAL, LINES };
+    enum CubeRenderModes cubeRenderMode;
+    double dFps;
+    Window* pWindow;
 
     MyApp(Window * win ) : App(win) {
         to = timeGetTime();
-        scene.camera.pos(0, 0, 10); 
+        scene.camera.pos(0, 0, 4); 
+        setupCube();
+        cubeRenderMode = NORMAL;
+        bDoAnimate = true;
+        bDrawSpheresAsWireframe = false;
+        bDrawCubeAsLines = false;
+        pWindow = win;
+    }
+
+    ~MyApp() {
+        if (pWindow) {
+            delete pWindow;
+        }
+    }
+
+    virtual void initGui() {
+        gui(bDoAnimate, "Toggle animation");
+        gui(bDrawSpheresAsWireframe, "Draw spheres wireframed");
+        gui(bDrawCubeAsLines, "Draw cube using lines within GA");
+    }
+
+    void setupCube() {
+        for (auto& i : cube.roots) {
+            auto tmp = Proj<DIM>::Ortho<3>(i);
+            vecCube.push_back(tmp);
+        }
     }
 
     void getMouse(){
@@ -52,57 +77,108 @@ struct MyApp : App {
     }
 
     void drawCube(Vec vecPosition, float fRadius) {
-        vector<Vec> proj;
-        static const int DIM = 4;
-
-        NCube<DIM> cube;
-
-        auto& res = cube.roots; 
-
-        for (auto& i : res) {
-            // For rotation:
-            //EGAMV<DIM, MV<24,12> > b(1,.01);
-            //i = i.sp(nrot(b));
-            
-            auto tmp = Proj<DIM>::Ortho<3>(i);
-            proj.push_back(tmp);
-        }
-
-        
-        for (auto i : cube.edges) {
-            gfx::Glyph::Line(proj[i.a], proj[i.b]);
+        for (auto edge : cube.edges) {
+            gfx::Glyph::Line(
+                vecCube[edge.a].rot(bivRotation),
+                vecCube[edge.b].rot(bivRotation)
+            );
         }
     }
 
+    void drawGACube(Vec vecPosition, float fRadius) {
+        Point bottomLeftFront = Ro::point(-fRadius, -fRadius, fRadius);
+        Point bottomRightFront = Ro::point(fRadius, -fRadius, fRadius);
+
+        Point topLeftFront = Ro::point(-fRadius, fRadius, fRadius);
+        Point topRightFront = Ro::point(fRadius, fRadius, fRadius);
+
+        Point bottomLeftBack = Ro::point(-fRadius, -fRadius, -fRadius);
+        Point bottomRightBack = Ro::point(fRadius, -fRadius, -fRadius);
+
+        Point topLeftBack = Ro::point(-fRadius, fRadius, -fRadius);
+        Point topRightBack = Ro::point(fRadius, fRadius, -fRadius);
+
+        Line lineTopBack = Fl::line(topLeftBack.rot(bivRotation), topRightBack.rot(bivRotation));
+        Line lineLeftBack = Fl::line(topLeftBack.rot(bivRotation), bottomLeftBack.rot(bivRotation));
+        Line lineRightBack = Fl::line(topRightBack.rot(bivRotation), bottomRightBack.rot(bivRotation));
+        Line lineBottomBack = Fl::line(bottomLeftBack.rot(bivRotation), bottomRightBack.rot(bivRotation));
+
+        Line lineTopLeft = Fl::line(topLeftBack.rot(bivRotation), topLeftFront.rot(bivRotation));
+        Line lineTopRight = Fl::line(topRightBack.rot(bivRotation), topRightFront.rot(bivRotation));
+        Line lineBottomLeft = Fl::line(bottomLeftBack.rot(bivRotation), bottomLeftFront.rot(bivRotation));
+        Line lineBottomRight = Fl::line(bottomRightBack.rot(bivRotation), bottomRightFront.rot(bivRotation));
+
+        Line lineTopFront = Fl::line(topLeftFront.rot(bivRotation), topRightFront.rot(bivRotation));
+        Line lineLeftFront = Fl::line(topLeftFront.rot(bivRotation), bottomLeftFront.rot(bivRotation));
+        Line lineRightFront = Fl::line(topRightFront.rot(bivRotation), bottomRightFront.rot(bivRotation));
+        Line lineBottomFront = Fl::line(bottomLeftFront.rot(bivRotation), bottomRightFront.rot(bivRotation));
+
+        Draw(lineTopBack);
+        Draw(lineLeftBack);
+        Draw(lineRightBack);
+        Draw(lineBottomBack);
+
+        Draw(lineTopLeft);
+        Draw(lineTopRight);
+        Draw(lineBottomLeft);
+        Draw(lineBottomRight);
+
+        Draw(lineTopFront);
+        Draw(lineLeftFront);
+        Draw(lineRightFront);
+        Draw(lineBottomFront);
+    }
+
     virtual void onDraw() {
-        fTime = 0.001f * (timeGetTime() - to);
+        std::stringstream strTitle;
+        strTitle << "Funkeeh stuff - FPS: ";
+        strTitle << pWindow->fpsActual();
+        pWindow->title(strTitle.str());
+
+        float fSphereRadius = 0.2;
+
+        if (bDrawSpheresAsWireframe) {
+            fSphereRadius *= (-1);
+        }
+
+        if (bDoAnimate) {
+            fTime = (timeGetTime() - to);
+        }
+        else {
+            fTime = 0.0f;
+        }
+
         getMouse();
+        bivRotation = Biv(sinf(fTime * 0.001f), 0, cosf(fTime * 0.001f));
 
-        Point a = Ro::point(0, 0, 0);
-        Draw(a);
+        
+        // Individual rotation of the spheres...
+        glRotatef(fTime * 0.1f, 1.0f, 0.0f, 1.0f);
 
-        auto foo = Vec(-0.5, 0.5, -0.5);
-        Biv rotX(fTime, 0, 0);
-
-        auto sbtl = Ro::sphere(foo.rot(rotX), 0.2);
+        auto sbtl = Ro::sphere(Vec(-0.5, 0.5, -0.5).rot(bivRotation), fSphereRadius);
         Draw(sbtl);
-        auto sftl = Ro::sphere(Vec(-0.5, 0.5, 0.5), 0.2);
+        auto sftl = Ro::sphere(Vec(-0.5, 0.5, 0.5).rot(bivRotation), fSphereRadius);
         Draw(sftl);
-        auto sbtr = Ro::sphere(Vec(0.5, 0.5, -0.5), 0.2);
+        auto sbtr = Ro::sphere(Vec(0.5, 0.5, -0.5).rot(bivRotation), fSphereRadius);
         Draw(sbtr);
-        auto sftr = Ro::sphere(Vec(0.5, 0.5, 0.5), 0.2);
+        auto sftr = Ro::sphere(Vec(0.5, 0.5, 0.5).rot(bivRotation), fSphereRadius);
         Draw(sftr);
 
-        auto sbbl = Ro::sphere(Vec(-0.5, -0.5, -0.5), 0.2);
+        auto sbbl = Ro::sphere(Vec(-0.5, -0.5, -0.5).rot(bivRotation), fSphereRadius);
         Draw(sbbl);
-        auto sfbl = Ro::sphere(Vec(-0.5, -0.5, 0.5), 0.2);
+        auto sfbl = Ro::sphere(Vec(-0.5, -0.5, 0.5).rot(bivRotation), fSphereRadius);
         Draw(sfbl);
-        auto sbbr = Ro::sphere(Vec(0.5, -0.5, -0.5), 0.2);
+        auto sbbr = Ro::sphere(Vec(0.5, -0.5, -0.5).rot(bivRotation), fSphereRadius);
         Draw(sbbr);
-        auto sfbr = Ro::sphere(Vec(0.5, -0.5, 0.5), 0.2);
+        auto sfbr = Ro::sphere(Vec(0.5, -0.5, 0.5).rot(bivRotation), fSphereRadius);
         Draw(sfbr);
 
-        drawCube(Vec(0, 0, 0), 2.0f);
+        if (bDrawCubeAsLines) {
+            drawGACube(Vec(0, 0, 0), 0.5f);
+        }
+        else {
+            drawCube(Vec(0, 0, 0), 0.5f);
+        }
     }
 };
 
@@ -111,8 +187,12 @@ MyApp * app;
 
 int main() {
     GLV glv(0, 0);
-    Window * win = new Window(500, 500, "Funkeeh stuff!", &glv);
+    
+    Window * win = new Window(1024, 768, "Funkeeh stuff!", &glv);
+    
     app = new MyApp(win); 
+    app->initGui();
+
     glv << *app;
 
     Application::run();
